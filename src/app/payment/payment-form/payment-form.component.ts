a2fb2d6542c1fa29ccaa170cff4b5063353c27a6
ShymaaRagab <102189654+ShymaaRagab@users.noreply.github.com>
2024-10-16 14:23:44 +0300

    Sh_R

--- src/app/payment/payment-form/payment-form.component.ts
+++ src/app/payment/payment-form/payment-form.component.ts
@@ -9,17 +9,18 @@ import { Router } from '@angular/router';
 })
 export class PaymentFormComponent {
   errorMessage: string | undefined;
-  orederId:number = 0;
+  order = JSON.parse(localStorage.getItem('orderSummary') || '{}');
+  orederId:number = this.order.orderID;
   constructor(private router: Router, private CartService:CartServiceService){}
-  
+
 
   onSubmit() {
     if(this.CartService.getCartsData().length>0){
       this.navigateToPath('payment-success');
       const order = localStorage.setItem('orderSummary',JSON.stringify({
-        orderID: this.orederId + 1, 
-        totalPrice: this.CartService.totalPriceInCart(), 
-        purchaseDate: new Date() 
+        orderID: this.orederId + 1,
+        totalPrice: this.CartService.totalPriceInCart(),
+        purchaseDate: new Date()
       }));
       this.CartService.clearCart();
     }else{
